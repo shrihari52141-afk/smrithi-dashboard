ed3cdc3eeecc11c9ad78840eb9766c498a2666a0
shrihari52141-afk <shrihari52141@gmail.com>
2026-06-18 12:05:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
